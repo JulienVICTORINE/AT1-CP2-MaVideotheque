4f3e48a56f87016966748550d68c57aaebe1086d
julien <julien.victorine440@gmail.com>
2025-04-15 23:09:24 +0400

    trier et afficher les films les mieux et les moins biens notés

--- script.js
+++ script.js
@@ -2,11 +2,13 @@
 const url = "https://ghibliapi.vercel.app/films?limit=50";
 const main = document.querySelector("main");
 
-// les boutons
+// je récupère l'id des boutons : affiche dans l'ordre croissant ou décroissant
 const btnAz = document.querySelector("#btnSortAz");
 const btnZa = document.querySelector("#btnSortZa");
-// const btnNoteAsc = document.querySelector("#btnNoteAsc");
-// const btnNoteDesc = document.querySelector("#btnNoteDesc");
+
+// je récupère l'id des boutons : affiche les films les plus ou moins notés
+const btnNoteAsc = document.querySelector("#btnNoteAsc");
+const btnNoteDesc = document.querySelector("#btnNoteDesc");
 
 var movies = [];
 var sortMethod = "";
@@ -20,28 +22,44 @@ const fetchMovies = async () => {
 // J'affiche tous les données
 const updateMain = () => {
   main.innerHTML = ""; // je vide le main
+  let filteredMovies = [...movies]; // je fais une copie
 
-  movies
-    .sort((a, b) => {
-      if (sortMethod == "az") return a.title.localeCompare(b.title);
-      else if (sortMethod == "za") return b.title.localeCompare(a.title);
-    })
+  // Appliquer un filtre selon le bouton cliqué
+  if (sortMethod === "noteAsc") {
+    // On garde que les films les moins bien notés
+    filteredMovies = filteredMovies.filter((m) => m.rt_score <= 79);
+  } else if (sortMethod === "noteDesc") {
+    // On garde que les films les mieux notés
+    filteredMovies = filteredMovies.filter((m) => m.rt_score >= 80);
+  }
 
-    .map((movie) => {
-      main.innerHTML += `
+  // On trie les films filtrés
+  filteredMovies.sort((a, b) => {
+    if (sortMethod == "az") return a.title.localeCompare(b.title);
+    else if (sortMethod == "za") return b.title.localeCompare(a.title);
+    else if (sortMethod == "noteAsc") return a.rt_score - b.rt_score;
+    else if (sortMethod == "noteDesc") return b.rt_score - a.rt_score;
+  });
+
+  // On affiche les films
+  filteredMovies.map((movie) => {
+    main.innerHTML += `
         <div class="card">
             <img src="${movie.image}" alt="image du film ${movie.title}" />
             <h3>${movie.title}</h3>
-            <p>Note : ${movie.rt_score / 20} / 5</p>
+            <p>Note : ${movie.rt_score} (${movie.rt_score / 20}) / 5</p>
             <p>${movie.description}</p>
         </div>
         `;
-    });
+  });
 };
 
 fetchMovies();
 
 // je créé un événeemnt pour mes boutons
+///////////////////
+// Pour afficher les films dans l'ordre croissant ou décroissant
+///////////////////
 btnAz.addEventListener("click", () => {
   sortMethod = "az";
   updateMain();
@@ -49,5 +67,18 @@ btnAz.addEventListener("click", () => {
 
 btnZa.addEventListener("click", () => {
   sortMethod = "za";
+  updateMain();
+});
+
+///////////////////
+// Pour affiche les films les biens ou moins notés
+//////////////////
+btnNoteAsc.addEventListener("click", () => {
+  sortMethod = "noteAsc";
+  updateMain();
+});
+
+btnNoteDesc.addEventListener("click", () => {
+  sortMethod = "noteDesc";
   updateMain();
 });
